baaf2639e329e2ba72307274c5daa25c59915186
Eworitse <ochukoeworitsekolo@gmail.com>
2020-11-05 16:15:20 +0100

    Updates

---
+++ precache-manifest.fff2635b810f8a690d25d0cf3eed45eb.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e715e03578df9c641cf3ad2c1de107",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "465f1c2b61b8b8cf50cc",
+    "url": "/portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "465f1c2b61b8b8cf50cc",
+    "url": "/portfolio/static/js/2.0dd10cd4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.0dd10cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d8354e368cb4fdfeaf",
+    "url": "/portfolio/static/js/main.b8bcb025.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ef755211ef3eb198efcfd7faebac39c6",
+    "url": "/portfolio/static/media/banner.ef755211.jpg"
+  },
+  {
+    "revision": "d0031dd00a5fd71f3c81d7a6b89a7343",
+    "url": "/portfolio/static/media/boldstep.d0031dd0.jpg"
+  },
+  {
+    "revision": "afd5cc174afc04810c162bb99a7822de",
+    "url": "/portfolio/static/media/bootstrap.afd5cc17.svg"
+  },
+  {
+    "revision": "eeb867b499bff52f224e8153431e4b2b",
+    "url": "/portfolio/static/media/close.eeb867b4.svg"
+  },
+  {
+    "revision": "6ef5b600a04e4633db84459f80224e59",
+    "url": "/portfolio/static/media/corel.6ef5b600.svg"
+  },
+  {
+    "revision": "35053a84281c5970c49e8cf83b27be7c",
+    "url": "/portfolio/static/media/css.35053a84.svg"
+  },
+  {
+    "revision": "7bcf994cd4aaa5ebae8bcf344a43757e",
+    "url": "/portfolio/static/media/express.7bcf994c.svg"
+  },
+  {
+    "revision": "557ab440ca9f5afecfdf3f02b4e7b2cf",
+    "url": "/portfolio/static/media/fb-white.557ab440.svg"
+  },
+  {
+    "revision": "91c90df58e6c9fb3755dcbf60ed74de1",
+    "url": "/portfolio/static/media/figma.91c90df5.svg"
+  },
+  {
+    "revision": "647e61eea1957644ad27270bf6185a7c",
+    "url": "/portfolio/static/media/git.647e61ee.svg"
+  },
+  {
+    "revision": "beab452654f684e966bf0d670b6b105b",
+    "url": "/portfolio/static/media/github-white.beab4526.svg"
+  },
+  {
+    "revision": "810f9094321053751d8f31100a1f0a35",
+    "url": "/portfolio/static/media/github.810f9094.svg"
+  },
+  {
+    "revision": "8842eba1e2ed84d4966e0192012acc36",
+    "url": "/portfolio/static/media/groomfield.8842eba1.jpg"
+  },
+  {
+    "revision": "fdac6e2cf073b113d1d7c439163f2460",
+    "url": "/portfolio/static/media/hamburger.fdac6e2c.svg"
+  },
+  {
+    "revision": "46fac4e079299dd778b2c688192c3a5d",
+    "url": "/portfolio/static/media/html.46fac4e0.svg"
+  },
+  {
+    "revision": "b3bf11178de0b8829ee4f24ca7b091c7",
+    "url": "/portfolio/static/media/java.b3bf1117.svg"
+  },
+  {
+    "revision": "402e511f8de2f49cbb4e1bd4c4f34335",
+    "url": "/portfolio/static/media/js.402e511f.svg"
+  },
+  {
+    "revision": "eeb3d512dfe986cfa6547de85b34c9f8",
+    "url": "/portfolio/static/media/logo.eeb3d512.svg"
+  },
+  {
+    "revision": "2f74065efe1f7547e3883e4b3f56c567",
+    "url": "/portfolio/static/media/mongodb.2f74065e.svg"
+  },
+  {
+    "revision": "18a9c0022760056ed06ecc2488227689",
+    "url": "/portfolio/static/media/node.18a9c002.svg"
+  },
+  {
+    "revision": "49d0d35f38bf9111498c3e1fbc8e47dd",
+    "url": "/portfolio/static/media/phone-white.49d0d35f.svg"
+  },
+  {
+    "revision": "514c8f960448a7f8b3ab49255c66a08e",
+    "url": "/portfolio/static/media/ps.514c8f96.svg"
+  },
+  {
+    "revision": "b0cc7b19dd09275b89b62b43584052b0",
+    "url": "/portfolio/static/media/react.b0cc7b19.svg"
+  },
+  {
+    "revision": "c34b44856d5e7fe9d63a3c669b34f50b",
+    "url": "/portfolio/static/media/redux.c34b4485.svg"
+  },
+  {
+    "revision": "2b7285f8d3a92ddb21a54a528d65bf47",
+    "url": "/portfolio/static/media/sass.2b7285f8.svg"
+  },
+  {
+    "revision": "ef5949b59fab5f5739ae030b8885a8a3",
+    "url": "/portfolio/static/media/soundrave.ef5949b5.jpg"
+  },
+  {
+    "revision": "1a9c86a8c8ca0e4a34223e7a10ee0f57",
+    "url": "/portfolio/static/media/twitter-white.1a9c86a8.svg"
+  }
+]);
